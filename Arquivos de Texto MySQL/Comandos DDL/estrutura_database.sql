26e0c24a454ff67028605ad6317f8f9141322663
saydaires <saydaires@gmail.com>
2024-11-13 09:38:53 -0300

    Refatoração estrutura_database.sql
    A tabela itens_pedidos que possui uma FOREIGN KEY para id_pedido da tabela pedidos, passa a ter restrição ON DELETE CASCADE. O que significa que sempre que um pedido da tabela pedidos for excluído, um registro de itens_pedidos será excluído automaticamente por efeito cascata.

--- Arquivos de Texto MySQL/Comandos DDL/estrutura_database.sql
+++ Arquivos de Texto MySQL/Comandos DDL/estrutura_database.sql
@@ -49,7 +49,7 @@ CREATE TABLE itens_pedidos(
     quantidade INT NOT NULL,
     preco_unitario DECIMAL(10, 2) NOT NULL,
     valor_total DECIMAL(10, 2) NOT NULL, -- o valor total varia entre quantidade * preco_unitario ou (quantidade * preco_unitario) - desconto
-    FOREIGN KEY(id_pedido) REFERENCES pedidos(id_pedido),
+    FOREIGN KEY(id_pedido) REFERENCES pedidos(id_pedido) ON DELETE CASCADE, -- refatoracao
     FOREIGN KEY(id_produto) REFERENCES produtos(id_produto)
 );
 
