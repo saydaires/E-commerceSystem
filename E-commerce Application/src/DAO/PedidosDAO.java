c281ebe58abdc29522a293d945dfbdd97d88bb07
saydaires <saydaires@gmail.com>
2024-11-13 16:38:56 -0300

    Atualizações em PedidosDAO
    Novos métodos e importação de novas classes.

--- E-commerce Application/src/DAO/PedidosDAO.java
+++ E-commerce Application/src/DAO/PedidosDAO.java
@@ -9,6 +9,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 import MODEL.ENUM.StatusPedido;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PedidosDAO {
     public static void insertPedido(PedidosMODEL pedido) throws ParseException {
@@ -64,8 +66,53 @@ public class PedidosDAO {
         }
     }
     
-    //listPedidos
-    // deletePedido
+    public static List<PedidosMODEL> listPedidos() {
+        List<PedidosMODEL> pedidos = new ArrayList<>();
+        try {
+            Connection conn = ConnectionUTIL.connectDB();
+            String sql = "SELECT * FROM pedidos";
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+            while(rs.next()) {
+                java.sql.Date dataPedidoSQL = rs.getDate("data_pedido");
+                SimpleDateFormat formatador = new SimpleDateFormat("yyyy-MM-dd");
+                String dataPedido = formatador.format(dataPedidoSQL);
+                
+                String statusPedidoSQL = rs.getString("status_pedido");
+                StatusPedido statusPedido = null;
+                switch(statusPedidoSQL) {
+                    case "PENDENTE":
+                        statusPedido = StatusPedido.PENDENTE;
+                        break;
+                    case "EM PROCESSAMENTO":
+                        statusPedido = StatusPedido.EM_PROCESSAMENTO;
+                        break;
+                    case "ENVIADO":
+                        statusPedido = StatusPedido.ENVIADO;
+                }
+                PedidosMODEL pedido = new PedidosMODEL(rs.getInt("id_cliente"), dataPedido, statusPedido);
+                pedido.setIdPedido(rs.getInt("id_pedido"));
+                pedidos.add(pedido);
+            }
+        } catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return pedidos;
+    }
+    
+    public static void deletePedido(int id_pedido) {
+        try {    
+            Connection conn = ConnectionUTIL.connectDB();
+            String sql = "DELETE FROM pedidos WHERE id_pedido = ?";
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, id_pedido);
+            pstm.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Pedido DELETADO com suceSSO!");
+        } catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
     // updatStatusPedido --> Criar uma trigger que exclua o pedido se o status for alterado para ENTREGUE
    
 }
