36368a6dc832f805b18d1eff20dd844ec78ef0c3
saydaires <saydaires@gmail.com>
2024-11-18 08:11:52 -0300

    VIEW Administrador
    Criada a tela de Login de funcionário (com a validação do Login já implementada) e a chamada para o menu de administrador em caso de login bem sucedido.

---
+++ E-commerce Application/src/SERVICE/ADMIN/Login.java
@@ -0,0 +1,31 @@
+package SERVICE.ADMIN;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import UTIL.ConnectionUTIL;
+import javax.swing.JOptionPane;
+
+public class Login {
+    public static boolean validarLogin(String email, String senha) {
+        try {    
+            Connection conn = ConnectionUTIL.connectDB();
+            String sql = "SELECT senha FROM funcionarios WHERE email = ?";
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setString(1, email);
+            ResultSet rs = pstm.executeQuery();
+            if(!rs.isBeforeFirst()) {
+                JOptionPane.showMessageDialog(null, "Email ou Senha invalidos!");
+                return false;
+            }
+            rs.first();
+            String senhaBD = rs.getString("senha");
+            if(senhaBD.equals(senha))
+                return true;
+        
+        } catch(SQLException e) {
+            //JOptionPane.showMessageDialog(null, e.getMessage()); -- teste
+        }
+        return false;
+    }
+}
